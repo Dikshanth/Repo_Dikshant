af7873c7d5594ab27555ef72707be06ea691cfe4
Dikshanth <138885832+Dikshanth@users.noreply.github.com>
2023-07-11 15:50:32 +0530

    Commit
    Reverse

--- Git_Project/src/github_Project/First.java
+++ Git_Project/src/github_Project/First.java
@@ -1,9 +1,25 @@
 package github_Project;
 
+import java.util.Scanner;
+
 public class First {
 
 	public static void main(String[] args) {
-     System.out.println("Hello hi");
-	}
+		 Scanner sc = new Scanner(System.in);
+		    System.out.println("Enter a number");
+		    int num = sc.nextInt();
+		    int n=num,rev=0,mod;
+		    while(n>0) {
+		    	mod=n%10;
+		    	rev=rev*10+mod;
+		    	n=n/10;
+		    }
+		    System.out.println("The reverse of "+num+" is "+rev);
+		    sc.close();
+			}
 
-}
+		}
+
+	
+
+
