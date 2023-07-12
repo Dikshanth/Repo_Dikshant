5325c06fb0cf2c91b267361c4dc9ca911bbf2deb
Dikshanth <138885832+Dikshanth@users.noreply.github.com>
2023-07-12 12:33:11 +0530

    commit2
    c2

---
+++ Git_Project/src/github_Project/Perfect_nu.java
@@ -0,0 +1,29 @@
+package github_Project;
+
+import java.util.Scanner;
+
+public class Perfect_nu {
+
+	public static void main(String[] args) {
+		  Scanner sc = new Scanner(System.in);
+		    System.out.println("Enter any number");
+			int n=sc.nextInt();
+			int sum=0;
+			for(int i=1;i<n;i++) {
+				if(n%i==0) {
+					sum=sum+i;
+				}
+			}
+			if(n==sum) {
+				System.out.println("The entered number is a perfect number");
+			}
+			else {
+				System.out.println("The entered number is not a perfect number");
+				sc.close();
+			}
+			
+			}
+
+	}
+
+
