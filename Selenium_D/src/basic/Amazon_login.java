066f8135dfca37928600f2d58537477e7b9796bc
Dikshanth <138885832+Dikshanth@users.noreply.github.com>
2023-08-21 15:08:16 +0530

    Amazon search commit
    c4

---
+++ Selenium_D/src/basic/Amazon_login.java
@@ -0,0 +1,26 @@
+package basic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazon_login {
+
+	public static void main(String[] args) throws InterruptedException {
+    WebDriver driver = new ChromeDriver ();
+    driver.get("https://www.amazon.in/");
+    driver.manage().window().maximize();
+    By by_search= By.xpath("//input[@type=\"text\"]");
+    WebElement ele_search=driver.findElement(by_search);
+    ele_search.sendKeys("mobile");
+    
+    By by_clicksearch =By.xpath("//input[@type=\"submit\"]");
+    WebElement ele_clicksearch=driver.findElement(by_clicksearch);
+    ele_clicksearch.click();
+    
+    Thread.sleep(1000);
+    driver.close();
+	}
+
+}
