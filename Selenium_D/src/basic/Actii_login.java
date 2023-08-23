497686f818d91d8ae1a938a6b98f8a3e5dbbbb35
Dikshanth <138885832+Dikshanth@users.noreply.github.com>
2023-08-23 10:48:38 +0530

    Actii_program
    c1

--- Selenium_D/src/basic/Actii_login.java
+++ Selenium_D/src/basic/Actii_login.java
@@ -1,15 +1,16 @@
 package basic;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Actii_login 
 {
 
 	public static void main(String[] args) throws InterruptedException 
 	{
+		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://demo.actitime.com/login.do");
 		driver.manage().window().maximize();
