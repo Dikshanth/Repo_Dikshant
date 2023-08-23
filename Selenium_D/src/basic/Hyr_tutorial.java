951228cb4c2d0908576995ca3e2219bb20da9ec6
Dikshanth <138885832+Dikshanth@users.noreply.github.com>
2023-08-23 12:17:31 +0530

    hyr tutoraial
    c2

---
+++ Selenium_D/src/basic/Hyr_tutorial.java
@@ -0,0 +1,42 @@
+package basic;
+
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hyr_tutorial {
+
+	public static void main(String[] args) throws InterruptedException {
+		{
+		    WebDriver driver = new ChromeDriver ();
+		    driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
+		    driver.manage().window().maximize();
+		    //singlewindow
+		    
+		    String parentwindowhandle=driver.getWindowHandle();
+		    System.out.println("parent window handle"+parentwindowhandle+driver.getTitle());
+		    //driver.findElement(By.xpath("//button[@onclick=\"newBrowserWindow()\"]")
+		    By by_search= By.xpath("//button[@onclick=\"newBrowserWindow()\"]");
+		    WebElement ele_search=driver.findElement(by_search);
+		    ele_search.click();
+		    Set <String> windowhandles=driver.getWindowHandles();
+		    for(String windowhandle:windowhandles)
+		    {
+		    	if(!(parentwindowhandle.equals(windowhandles)))
+		    	{
+		    		driver.switchTo().window(windowhandle);
+		    		System.out.println(windowhandle+driver.getTitle());
+		    	}
+		    }
+		  
+		    
+		   
+		   
+		    
+	}
+
+}
+}
