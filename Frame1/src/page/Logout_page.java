d5105375c226c03b03677861b4fe2034903c3e77
Dikshanth <138885832+Dikshanth@users.noreply.github.com>
2023-08-31 18:20:54 +0530

    Framework
    F1

---
+++ Frame1/src/page/Logout_page.java
@@ -0,0 +1,37 @@
+package page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Logout_page extends Login_page {
+	static By btn=By.xpath("//button[@id=\"react-burger-menu-btn\"]");
+	static By lgt=By.xpath("//a[@id=\"logout_sidebar_link\"]");
+	
+	
+	
+	
+	public static WebElement bton() 
+	{
+		WebElement bn=drv.findElement(btn);
+		return bn;
+		
+	}
+	
+	public static WebElement lgot() throws InterruptedException 
+	{
+		Thread.sleep(2000);
+		WebElement logout=drv.findElement(lgt);
+		return logout;
+		
+	}
+	public static void action_btn() 
+	{
+		bton().click();
+		
+	}
+	
+	public static void action_logout() throws InterruptedException 
+	{
+		lgot().click();
+	}
+}
---
+++ Frame1/src/selenium_utility/Basic_operations.java
@@ -0,0 +1,48 @@
+package selenium_utility;
+
+//package Selnium_Utility;
+
+import java.time.Duration;
+import java.util.Set;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+//import page.facebook;
+
+public class Basic_operations 
+{
+	protected static WebDriver drv;
+	public static WebDriverWait wait;
+
+	public static void browser()
+	{	
+		drv=new ChromeDriver();
+		drv.get("https://www.saucedemo.com/");
+		drv.manage().window().maximize();
+		drv.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+	}
+	public static void explicity()
+	{
+		wait=new WebDriverWait(drv,Duration.ofSeconds(10));
+
+	}
+
+	public static void windowhandle() throws InterruptedException 
+	{
+		//facebook.fcebk().click();
+		Thread.sleep(2000);
+		String ename=drv.getWindowHandle();
+		System.out.println("base page element-"+ename);
+		Set<String>allwindows=drv.getWindowHandles();
+		for(String x:allwindows) 
+		{
+			if(!ename.equals(x)) 
+			{
+				drv.switchTo().window(x);
+			}
+		}
+	}
+}
